4bc3662ebc47224ae41e83339710311819b6f712
Shilpa3107 <shilpa.sinha3107@gmail.com>
2025-03-16 03:57:25 +0530

    Added root route for testing

--- main.py
+++ main.py
@@ -7,6 +7,21 @@ from crud import create_user, get_users
 
 app = FastAPI()
 
+@app.get("/")
+def root():
+    return {"message": "API is running"}
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows requests from any origin
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 # Initialize DB
 Base.metadata.create_all(bind=engine)
 
@@ -18,10 +33,10 @@ def get_db():
     finally:
         db.close()
 
-@app.post("/users/", response_model=UserResponse)
+@app.post("/users", response_model=UserResponse)
 def create_new_user(user: UserCreate, db: Session = Depends(get_db)):
     return create_user(db=db, user=user)
 
-@app.get("/users/", response_model=list[UserResponse])
+@app.get("/users", response_model=list[UserResponse])
 def read_users(db: Session = Depends(get_db)):
     return get_users(db=db)
